86440e764b923b1f409ff39d4a90ee807f9b7d9d
max <mrmax99@gmail.com>
2025-11-30 13:31:48 -0800

    initial commit with new dart project

---
+++ bin/aoc2025.dart
@@ -0,0 +1,5 @@
+import 'package:aoc2025/aoc2025.dart' as aoc2025;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aoc2025.calculate()}!');
+}
---
+++ test/aoc2025_test.dart
@@ -0,0 +1,8 @@
+import 'package:aoc2025/aoc2025.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
